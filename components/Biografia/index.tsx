e4d0bba5698ea02af6bd822ef53ed5cb766af404
Vater Santos <69923057+Valtersystem@users.noreply.github.com>
2022-08-09 13:28:32 +0100

    Update index.tsx

--- components/Biografia/index.tsx
+++ components/Biografia/index.tsx
@@ -12,7 +12,10 @@ export function Biografia() {
      <section>
         <BiografiaItem
         description= {<span>
-       Prazer em te conhecer, meu nome é Valter Santos. ⁠Antes de me apaixonar por programação, trabalhei em um hospital com fotografia científica e sempre tive vontade de aprender a criar web-sites, com esta vontade, comecei a pesquisar e me aprofundar no mundo da tecnologia. E então decidi realizar minha graduação em análise desenvolvimento de sistemas. No ano de 2022 tive a minha primeira oportunidade de trabalhar por 6 meses como desenvolvedor web Júnior, onde consegui experiência com algumas ferramentas: Vuejs, Git, Bootstrap, JavaScript e outras. ⁠ <br/> ⁠Falando um pouco mais sobre mim, gosto de jogar futebol, cozinhar e comer amendoim, e gosto de me divertir com amigos e familiares e às vezes também assisto a algumas séries.
+         Prazer em te conhecer, meu nome é Valter Santos. ⁠Antes de me apaixonar por programação, trabalhei no hospital de oncologia da América Latina (Hospital de Amor) como desenvolvedor de bancos em REDCap e fotografia científica e sempre tive vontade de aprender a criar web-sites, com esta vontade, comecei a pesquisar e me aprofundar no mundo da tecnologia. E então decidi realizar minha graduação em análise e desenvolvimento de sistemas. No ano de 2022 tive a minha primeira oportunidade de trabalhar por 6 meses como desenvolvedor web Júnior, onde consegui experiência com algumas ferramentas: Vuejs, Git, Bootstrap, JavaScript e outras. ⁠ <br/> ⁠
+
+Falando um pouco mais sobre mim, gosto de jogar futebol, cozinhar e também de me divertir com amigos e familiares e às vezes assisto a algumas séries.
+
          </span>}
         />
      </section>
